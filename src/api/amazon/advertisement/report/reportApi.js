53ad1f63f3e0687b468454cf3db87964e4992758
Wimoor 万墨ERP <felix.liu.hn@gmail.com>
2025-11-28 12:51:36 +0800

    深圳市万墨信息科技有限公司
    删除旧项目

--- src/api/amazon/advertisement/report/reportApi.js
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/request.js";
-function getsumproduct(data){
-	 return request.post('/amazonadv/api/v1/advReport/getsumproduct',data);
-}
-
-export default{
-	getsumproduct,
- 
-}
--- src/api/quote/purchaseApi.js
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/request.js";
- 
-function save(data){
-	return request.post('/quote/api/v1/quote/purchase/save',data);
-}
- 
- export default{
- 	 save
- }
